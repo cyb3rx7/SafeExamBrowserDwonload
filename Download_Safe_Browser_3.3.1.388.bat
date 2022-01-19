b5e969cacc1decf3d88b7201bc597aa579c67cf2
cyb3rx7n <54465100+cyb3rx7n@users.noreply.github.com>
2022-01-19 20:51:57 +0200

    Update Download_Safe_Browser_3.3.1.388.bat

--- Download_Safe_Browser_3.3.1.388.bat
+++ Download_Safe_Browser_3.3.1.388.bat
@@ -1,15 +1,15 @@
 @echo off
 
-echo Create Folder on Desktop
+echo "Create Folder on Desktop with name SafeExamBrowser"
 mkdir %USERPROFILE%\DESKTOP\SafeExamBrowser"
 
-echo Open Folder on Desktop
+echo "Open Folder on Desktop name SafeExamBrowser"
 cd  %USERPROFILE%\DESKTOP\SafeExamBrowser"
 
-echo Download SafeExamBrowser_3.3.1.388
+echo "Download SafeExamBrowser_3.3.1.388.exe it's 194.234 MB maybe take some tiime so pleses wait"
 curl "https://moodle2.bgu.ac.il/moodle/pluginfile.php/2319634/course/section/532495/SEB_3.3.1.388.exe" --output SEB_3.3.1.388.exe
 
-echo Download SebClientSettings
+echo "Download SebClientSettings.seb it's 2.93 KB"
 curl "https://moodle2.bgu.ac.il/moodle/pluginfile.php/2625864/mod_resource/content/4/SebClientSettings.seb" --output SebClientSettings.seb
 
 pause
