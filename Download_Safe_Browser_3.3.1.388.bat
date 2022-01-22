1f0b568026f0218509926ca9b225359df25dcfbd
cyb3rx7n <54465100+cyb3rx7n@users.noreply.github.com>
2022-01-22 02:36:50 +0200

    Update Download_Safe_Browser_3.3.1.388.bat
    to be less line & change C:\Program Files with %PROGRAMFILES%

--- Download_Safe_Browser_3.3.1.388.bat
+++ Download_Safe_Browser_3.3.1.388.bat
@@ -1,55 +1,46 @@
 @echo off
-
+title Install SafeExamBrowser
 :: BatchGotAdmin
 :-------------------------------------
 REM  --> Check for permissions
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
 REM --> If error flag set, we do not have admin.
 if '%errorlevel%' NEQ '0' (
     echo Requesting administrative privileges...
     goto UACPrompt
 ) else ( goto gotAdmin )
-
 :UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
     set params = %*:"=""
     echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
     "%temp%\getadmin.vbs"
     del "%temp%\getadmin.vbs"
     exit /B
-
 :gotAdmin
     pushd "%CD%"
     CD /D "%~dp0"
 :--------------------------------------
-echo "Create Folder on Desktop with name SafeExamBrowser on Desktop"
-
+echo "Create Folder on Desktop with name and start downloads and install SafeExamBrowser on Desktop"
 mkdir %USERPROFILE%\DESKTOP\SafeExamBrowser"
-
-echo "Open Folder on Desktop name SafeExamBrowser"
-
 cd  %USERPROFILE%\DESKTOP\SafeExamBrowser"
-
-echo "Download SafeExamBrowser_3.3.1.388.exe it's 194.234 MB maybe take five minutes so please wait"
-
+echo.
+echo "Download SafeExamBrowser_3.3.1.388.exe it's 194.234 MB maybe take two minutes so please wait"
 curl "https://moodle2.bgu.ac.il/moodle/pluginfile.php/2319634/course/section/532495/SEB_3.3.1.388.exe" --output SEB_3.3.1.388.exe
-
+echo.
 echo "Download SebClientSettings.seb it's 2.93 KB"
-
 curl "https://moodle2.bgu.ac.il/moodle/pluginfile.php/2625864/mod_resource/content/4/SebClientSettings.seb" --output SebClientSettings.seb
-:--------------------------------------
-echo "install SafeExamBrowser to location C:\Program Files\SafeExamBrowser.exe"
-
-set "SafeExamBrowserInstallDir=C:\Program Files\SafeExamBrowser"
+echo.
+set "SafeExamBrowserInstallDir=%PROGRAMFILES%\SafeExamBrowser"
 %USERPROFILE%\DESKTOP\SafeExamBrowser\SEB_3.3.1.388.exe /silent /norestart TargetDir="%SafeExamBrowserInstallDir%"
-
-echo "open SebClientSettings.seb with SafeExamBrowser.exe"
-
-"C:\Program Files\SafeExamBrowser\Application\SafeExamBrowser.exe" -U user -d dbName -f "%USERPROFILE%\DESKTOP\SafeExamBrowser\SebClientSettings.seb"
-
-echo "copy SafeExamBrowser to Desktop"
-
+echo.
+cd %PROGRAMFILES%\SafeExamBrowser\Application\
+start SafeExamBrowser.exe %USERPROFILE%\DESKTOP\SafeExamBrowser\SebClientSettings.seb
+echo.
 cp -r "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Safe Exam Browser.lnk" "%USERPROFILE%\DESKTOP"
+echo.
+echo.
+echo "Thanks you "
+echo "have a Nice Day!"
+echo.
+echo.
 pause
